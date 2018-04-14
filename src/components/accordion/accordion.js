c6d6a4d612a4a5dd51d910ffdd59d6a4304b5495
Edgar Frías <edfriasji@gmail.com>
2018-04-14 19:59:32 +0200

    Corrections: linting code

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -3,58 +3,55 @@ import Panel from './panel';
 
 const panelsContent = [
   {
-    "heading": "Panel 1",
-    "content": "Panel 1 Content...",
+    heading: 'Panel 1',
+    content: 'Panel 1 Content...',
   },
   {
-    "heading": "Panel 2",
-    "content": "Panel 2 Content...",
+    heading: 'Panel 2',
+    content: 'Panel 2 Content...',
   },
   {
-    "heading": "Panel 3",
-    "content": "Panel 3 Content...",
-  }
-]
+    heading: 'Panel 3',
+    content: 'Panel 3 Content...',
+  },
+];
 
 class Accordion {
-  constructor(container, panelsContent) {
+  constructor(container) {
     this.panelsContent = panelsContent;
 
     panelsContent.forEach((panelData) => {
-      const panel = new Panel(panelData, container);
+      Panel(panelData, container);
     });
   }
 
   openPanel() {
     const accordionHeading = document.querySelectorAll('.accordion-header');
-    accordionHeading.forEach(item => {
+    accordionHeading.forEach((item) => {
       item.addEventListener('click', () => {
-          accordionHeading.forEach(element => {
-            element.classList.contains('active') ?
-                element.classList.remove('active') : null
-          });
+        accordionHeading.forEach((element) => {
+          element.classList.contains('active') && element.classList.remove('active');
+        });
 
-          item.classList.add('active');
+        item.classList.add('active');
       });
     });
   }
 
   // If you want to open all panels at the same time use this:
-   openAllPanels() {
+  openAllPanels() {
     const accordionHeading = document.querySelectorAll('.accordion-header');
-    accordionHeading.forEach((item, key) => {
-        item.addEventListener('click', () => {
-          accordionHeading.forEach(element => {
-            element.classList.contains('active') ?
-              element.classList.remove('active') :
-              element.classList.add('active');
-          });
+    accordionHeading.forEach((item) => {
+      item.addEventListener('click', () => {
+        accordionHeading.forEach((element) => {
+          element.classList.contains('active') ? element.classList.remove('active') : element.classList.add('active');
         });
+      });
     });
-   }
+  }
 }
 
-const container = document.querySelector(".accordion");
+const container = document.querySelector('.accordion');
 const accordion = new Accordion(container, panelsContent);
 accordion.openPanel();
 
